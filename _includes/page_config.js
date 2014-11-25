0cc0ab5389d42800ff123417b3affbb797b9dc73
James Conkling <james.lane.conkling@gmail.com>
2014-11-24 17:48:31 -0800

    rename mapLayers as pageConfig

---
+++ _includes/page_config.js
@@ -0,0 +1,13 @@
+{% comment %}
+if page js needs to access any frontmatter configuration, include parse_config.js
+in a html script before the main js file is executed
+currently used to allow specification of baselayer for each page
+{% endcomment %}{% assign mapBaseLayer = page.basemap.layers | first %}
+var pageConfig = {
+  baseLayer: {
+    id: '{{mapBaseLayer.id}}',
+    latlon: [{{page.basemap.center.lat}}, {{page.basemap.center.lon}}],
+    zoom: {{page.basemap.center.zoom}}
+  },
+  dataLayers: {}
+};
