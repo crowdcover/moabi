16c92b7f395e23ed82ceeb1830c3367e132f87a6
Kristofor Carle <kristoforcarle@moabi.org>
2015-11-05 17:11:37 -0500

    update base map

--- js/app/map-app.js
+++ js/app/map-app.js
@@ -19,7 +19,8 @@ function (moabi, L, leafletImage, leaflet_hash, leafletEasyPrint, $, sortable, j
     },
 
     buildMap: function(){
-      var baseLayer = L.tileLayer('http://tiles.osm.moabi.org/'+ pageConfig.baseLayer.id +'/{z}/{x}/{y}.png');
+      //var baseLayer = L.tileLayer('http://tiles.osm.moabi.org/'+ pageConfig.baseLayer.id +'/{z}/{x}/{y}.png');
+      var baseLayer = L.tileLayer('https://api.mapbox.com/v4/crowdcover.5572a3d7/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY3Jvd2Rjb3ZlciIsImEiOiI3akYtNERRIn0.uwBAdtR6Zk60Bp3vTKj-kg');
 
       L.mapbox.accessToken = 'pk.eyJ1IjoiamFtZXMtbGFuZS1jb25rbGluZyIsImEiOiJ3RHBOc1BZIn0.edCFqVis7qgHPRgdq0WYsA';
       this.map = L.mapbox.map('map', undefined, {
