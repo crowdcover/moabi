0b38feaa339705e04c12d791ec04d92be90bd5ea
Kristofor Carle <kris@maphubs.com>
2020-04-14 16:50:48 -0400

    upgrade to jekyll v3 to fix GitHub pages

--- _includes/page_config.js
+++ _includes/page_config.js
@@ -2,11 +2,11 @@
 if page js needs to access any frontmatter configuration, include parse_config.js
 in a html script tag before the main js file is executed
 currently used to allow specification of baselayer and map center for each page
-{% endcomment %}{% assign mapBaseLayer = page.basemap.layers | first %}
+{% endcomment %}{% assign mapBaseLayer = layout.basemap.layers | first %}
 var pageConfig = {
   baseLayer: {
     id: '{{mapBaseLayer.id}}',
-    latlon: [{{page.basemap.center.lat}} || -2.877, {{page.basemap.center.lon}} || 22.830],
-    zoom: {{page.basemap.center.zoom}} || 6
+    latlon: [{{layout.basemap.center.lat}}{{page.basemap.center.lat}}, {{page.basemap.center.lat}}{{layout.basemap.center.lon}}],
+    zoom: {{page.basemap.center.zoom}}{{layout.basemap.center.zoom}}
   }
 };
